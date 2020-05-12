7bb7c564fad56978e0912bf19cae93ab5368cd2a
Luiz Filipe Machado Barni <machado@odahcam.com>
2020-05-12 01:10:45 -0300

    fixed encoding-factory main exports

--- src/encoding/encoding-factory.ts
+++ src/encoding/encoding-factory.ts
@@ -63,37 +63,188 @@ const encodingIndexes = getEncodingIndexes();
 
 type XCoderFactory<Tncoder = Encoder | Decoder> = ({ fatal: boolean }) => Tncoder;
 
+type Encoders = {
+  [s: string]: XCoderFactory<Encoder>;
+};
+
+type Decoders = {
+  [s: string]: XCoderFactory<Decoder>;
+};
+
 // Registry of of encoder/decoder factories, by encoding name.
 /** @type {Object.<string, function({fatal:boolean}): Encoder>} */
-const encoders: { [s: string]: XCoderFactory<Encoder> } = {};
+// const encoders: Encoders = {};
 
 /** @type {Object.<string, function({fatal:boolean}): Decoder>} */
-const decoders: { [s: string]: XCoderFactory<Decoder> } = {};
+// const decoders: Decoders = {};
 
-// 9.1 utf-8
+  //
+  // 10. Legacy single-byte encodings
+  //
 
-// 9.1.1 utf-8 decoder
+  // 10.1 single-byte decoder
 
-// 9.1.2 utf-8 encoder
+  // 10.2 single-byte encoder
 
-/** @param {{fatal: boolean}} options */
-encoders['UTF-8'] = function (options: { fatal: boolean; }) {
-  return new UTF8Encoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['UTF-8'] = function (options: { fatal: boolean; }) {
-  return new UTF8Decoder(options);
-};
+export const encoders: Encoders = {
 
-//
-// 10. Legacy single-byte encodings
-//
+  // 9.1 utf-8
 
-// 10.1 single-byte decoder
+  // 9.1.1 utf-8 decoder
 
-// 10.2 single-byte encoder
+  // 9.1.2 utf-8 encoder
 
-if (encodingIndexes && encodingIndexes.length) {
+  /** @param {{fatal: boolean}} options */
+  'UTF-8': (options: { fatal: boolean; }) => new UTF8Encoder(options),
+
+  //
+  // 11. Legacy multi-byte Chinese (simplified) encodings
+  //
+
+  // 11.1 gbk
+
+  // 11.1.1 gbk decoder
+  // gbk's decoder is gb18030's decoder.
+
+  // 11.1.2 gbk encoder
+  // gbk's encoder is gb18030's encoder with its gbk flag set.
+  /** @param {{fatal: boolean}} options */
+  'GBK': (options: { fatal: boolean; }) => new GB18030Encoder(options, true),
+
+  // 11.2 gb18030
+  // 11.2.1 gb18030 decoder
+
+  // 11.2.2 gb18030 encoder
+
+  /** @param {{fatal: boolean}} options */
+  'gb18030': (options: { fatal: boolean; }) => new GB18030Encoder(options),
+
+
+
+  //
+  // 12. Legacy multi-byte Chinese (traditional) encodings
+  //
+
+  // 12.1 Big5
+
+  // 12.1.1 Big5 decoder
+
+  // 12.1.2 Big5 encoder
+
+  /** @param {{fatal: boolean}} options */
+  'Big5': (options: { fatal: boolean; }) => new Big5Encoder(options),
+
+
+  //
+  // 13. Legacy multi-byte Japanese encodings
+  //
+
+  // 13.1 euc-jp
+
+  // 13.1.1 euc-jp decoder
+
+  // 13.1.2 euc-jp encoder
+
+  /** @param {{fatal: boolean}} options */
+  'EUC-JP': (options: { fatal: boolean; }) => new EUCJPEncoder(options),
+
+  // 13.2 iso-2022-jp
+
+  // 13.2.1 iso-2022-jp decoder
+
+  // 13.2.2 iso-2022-jp encoder
+
+  /** @param {{fatal: boolean}} options */
+  'ISO-2022-JP': (options: { fatal: boolean; }) => new ISO2022JPEncoder(options),
+
+  // 13.3 Shift_JIS
+
+  // 13.3.1 Shift_JIS decoder
+
+  // 13.3.2 Shift_JIS encoder
+
+  /** @param {{fatal: boolean}} options */
+  'Shift_JIS': (options: { fatal: boolean; }) => new ShiftJISEncoder(options),
+
+  //
+  // 14. Legacy multi-byte Korean encodings
+  //
+
+  // 14.1 euc-kr
+
+  // 14.1.1 euc-kr decoder
+
+  // 14.1.2 euc-kr encoder
+
+  /** @param {{fatal: boolean}} options */
+  'EUC-KR': (options: { fatal: boolean; }) => new EUCKREncoder(options),
+
+
+  //
+  // 15. Legacy miscellaneous encodings
+  //
+
+  // 15.1 replacement
+
+  // Not needed - API throws RangeError
+
+  // 15.2 Common infrastructure for utf-16be and utf-16le
+
+  // 15.2.1 shared utf-16 decoder
+
+  // 15.2.2 shared utf-16 encoder
+
+  // 15.3 utf-16be
+  // 15.3.1 utf-16be decoder
+  /** @param {{fatal: boolean}} options */
+  'UTF-16BE': (options: { fatal: boolean; }) => new UTF16Encoder(true, options),
+  // 15.3.2 utf-16be encoder
+
+  // 15.4 utf-16le
+  // 15.4.1 utf-16le decoder
+  /** @param {{fatal: boolean}} options */
+  'UTF-16LE': (options: { fatal: boolean; }) => new UTF16Encoder(false, options),
+  // 15.4.2 utf-16le encoder
+
+  // 15.5 x-user-defined
+
+  // 15.5.1 x-user-defined decoder
+
+  // 15.5.2 x-user-defined encoder
+
+  /** @param {{fatal: boolean}} options */
+  'x-user-defined': (options: { fatal: boolean; }) => new XUserDefinedEncoder(options),
+}
+
+
+
+export const decoders: Decoders = {
+  /** @param {{fatal: boolean}} options */
+  'UTF-8': (options: { fatal: boolean; }) => new UTF8Decoder(options),
+  /** @param {{fatal: boolean}} options */
+  'GBK': (options: { fatal: boolean; }) => new GB18030Decoder(options),
+  /** @param {{fatal: boolean}} options */
+  'gb18030': (options: { fatal: boolean; }) => new GB18030Decoder(options),
+  /** @param {{fatal: boolean}} options */
+  'Big5': (options: { fatal: boolean; }) => new Big5Decoder(options),
+  /** @param {{fatal: boolean}} options */
+  'EUC-JP': (options: { fatal: boolean; }) => new EUCJPDecoder(options),
+  /** @param {{fatal: boolean}} options */
+  'ISO-2022-JP': (options: { fatal: boolean; }) => new ISO2022JPDecoder(options),
+  /** @param {{fatal: boolean}} options */
+  'Shift_JIS': (options: { fatal: boolean; }) => new ShiftJISDecoder(options),
+  /** @param {{fatal: boolean}} options */
+  'EUC-KR': (options: { fatal: boolean; }) => new EUCKRDecoder(options),
+  /** @param {{fatal: boolean}} options */
+  'UTF-16BE': (options: { fatal: boolean; }) => new UTF16Decoder(true, options),
+  /** @param {{fatal: boolean}} options */
+  'UTF-16LE': (options: { fatal: boolean; }) => new UTF16Decoder(false, options),
+  /** @param {{fatal: boolean}} options */
+  'x-user-defined': (options: { fatal: boolean; }) => new XUserDefinedDecoder(options),
+}
+
+
+if (encodingIndexes) {
   encodings.forEach(function (category) {
     if (category.heading !== 'Legacy single-byte encodings')
       return;
@@ -111,183 +262,3 @@ if (encodingIndexes && encodingIndexes.length) {
     });
   });
 }
-
-//
-// 11. Legacy multi-byte Chinese (simplified) encodings
-//
-
-// 11.1 gbk
-
-// 11.1.1 gbk decoder
-// gbk's decoder is gb18030's decoder.
-/** @param {{fatal: boolean}} options */
-decoders['GBK'] = function (options: { fatal: boolean; }) {
-  return new GB18030Decoder(options);
-};
-
-// 11.1.2 gbk encoder
-// gbk's encoder is gb18030's encoder with its gbk flag set.
-/** @param {{fatal: boolean}} options */
-encoders['GBK'] = function (options: { fatal: boolean; }) {
-  return new GB18030Encoder(options, true);
-};
-
-// 11.2 gb18030
-// 11.2.1 gb18030 decoder
-
-// 11.2.2 gb18030 encoder
-
-/** @param {{fatal: boolean}} options */
-encoders['gb18030'] = function (options: { fatal: boolean; }) {
-  return new GB18030Encoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['gb18030'] = function (options: { fatal: boolean; }) {
-  return new GB18030Decoder(options);
-};
-
-
-//
-// 12. Legacy multi-byte Chinese (traditional) encodings
-//
-
-// 12.1 Big5
-
-// 12.1.1 Big5 decoder
-
-// 12.1.2 Big5 encoder
-
-/** @param {{fatal: boolean}} options */
-encoders['Big5'] = function (options: { fatal: boolean; }) {
-  return new Big5Encoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['Big5'] = function (options: { fatal: boolean; }) {
-  return new Big5Decoder(options);
-};
-
-
-//
-// 13. Legacy multi-byte Japanese encodings
-//
-
-// 13.1 euc-jp
-
-// 13.1.1 euc-jp decoder
-
-// 13.1.2 euc-jp encoder
-
-/** @param {{fatal: boolean}} options */
-encoders['EUC-JP'] = function (options: { fatal: boolean; }) {
-  return new EUCJPEncoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['EUC-JP'] = function (options: { fatal: boolean; }) {
-  return new EUCJPDecoder(options);
-};
-
-// 13.2 iso-2022-jp
-
-// 13.2.1 iso-2022-jp decoder
-
-// 13.2.2 iso-2022-jp encoder
-
-/** @param {{fatal: boolean}} options */
-encoders['ISO-2022-JP'] = function (options: { fatal: boolean; }) {
-  return new ISO2022JPEncoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['ISO-2022-JP'] = function (options: { fatal: boolean; }) {
-  return new ISO2022JPDecoder(options);
-};
-
-// 13.3 Shift_JIS
-
-// 13.3.1 Shift_JIS decoder
-
-// 13.3.2 Shift_JIS encoder
-
-/** @param {{fatal: boolean}} options */
-encoders['Shift_JIS'] = function (options: { fatal: boolean; }) {
-  return new ShiftJISEncoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['Shift_JIS'] = function (options: { fatal: boolean; }) {
-  return new ShiftJISDecoder(options);
-};
-
-//
-// 14. Legacy multi-byte Korean encodings
-//
-
-// 14.1 euc-kr
-
-// 14.1.1 euc-kr decoder
-
-// 14.1.2 euc-kr encoder
-
-/** @param {{fatal: boolean}} options */
-encoders['EUC-KR'] = function (options: { fatal: boolean; }) {
-  return new EUCKREncoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['EUC-KR'] = function (options: { fatal: boolean; }) {
-  return new EUCKRDecoder(options);
-};
-
-
-//
-// 15. Legacy miscellaneous encodings
-//
-
-// 15.1 replacement
-
-// Not needed - API throws RangeError
-
-// 15.2 Common infrastructure for utf-16be and utf-16le
-
-// 15.2.1 shared utf-16 decoder
-
-// 15.2.2 shared utf-16 encoder
-
-// 15.3 utf-16be
-// 15.3.1 utf-16be decoder
-/** @param {{fatal: boolean}} options */
-encoders['UTF-16BE'] = function (options: { fatal: boolean; }) {
-  return new UTF16Encoder(true, options);
-};
-// 15.3.2 utf-16be encoder
-/** @param {{fatal: boolean}} options */
-decoders['UTF-16BE'] = function (options: { fatal: boolean; }) {
-  return new UTF16Decoder(true, options);
-};
-
-// 15.4 utf-16le
-// 15.4.1 utf-16le decoder
-/** @param {{fatal: boolean}} options */
-encoders['UTF-16LE'] = function (options: { fatal: boolean; }) {
-  return new UTF16Encoder(false, options);
-};
-// 15.4.2 utf-16le encoder
-/** @param {{fatal: boolean}} options */
-decoders['UTF-16LE'] = function (options: { fatal: boolean; }) {
-  return new UTF16Decoder(false, options);
-};
-
-// 15.5 x-user-defined
-
-// 15.5.1 x-user-defined decoder
-
-// 15.5.2 x-user-defined encoder
-
-/** @param {{fatal: boolean}} options */
-encoders['x-user-defined'] = function (options: { fatal: boolean; }) {
-  return new XUserDefinedEncoder(options);
-};
-/** @param {{fatal: boolean}} options */
-decoders['x-user-defined'] = function (options: { fatal: boolean; }) {
-  return new XUserDefinedDecoder(options);
-};
-
-export { decoders, encoders };
-
