d2820c339ffc9f4c50df1c50ca0e4a2e2487a5a3
Luiz Filipe Machado Barni <machado@odahcam.com>
2020-05-12 01:22:08 -0300

    polyfills

--- src/encoding.ts
+++ src/encoding.ts
@@ -1,2 +1,12 @@
-export * from './common/TextDecoder';
-export * from './common/TextEncoder';
+import { TextDecoder } from './common/TextDecoder';
+import { TextEncoder } from './common/TextEncoder';
+
+declare const window;
+
+// Polyfills browser
+if (typeof window !== 'undefined') {
+  if (!('TextDecoder' in window)) window['TextDecoder'] = TextDecoder;
+  if (!('TextEncoder' in window)) window['TextEncoder'] = TextEncoder;
+}
+
+export { TextDecoder, TextEncoder };
