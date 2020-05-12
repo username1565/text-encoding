5d4854fa5bf7a3e47e4721ab80a0df838108ae86
Luiz Filipe Machado Barni <machado@odahcam.com>
2020-05-12 02:01:46 -0300

    improved array equals

--- util/assert-package.js
+++ util/assert-package.js
@@ -4,8 +4,12 @@ const assert_equals = asserts.strictEqual;
 const assert_not_equals = asserts.notEqual;
 const assert_false = (a, msg) => asserts.strictEqual(a, false, msg);
 const assert_true = (a, msg) => asserts.strictEqual(a, true, msg);
-const assert_array_equals = (a, b) => Array.from(a).forEach((aItem, index) => assert_equals(aItem, b[index]));
 const assert_throws = (type, func) => expect(func).toThrow(type);
+const assert_array_equals = (a, b) =>  () => {
+  const aArr = Array.from(a);
+  assert_equals(aArr.length, b.length);
+  aArr.forEach((aItem, index) => assert_equals(aItem, b[index]));
+}
 
 module.exports = {
     assert_equals,
